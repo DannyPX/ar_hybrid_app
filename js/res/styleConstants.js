7085477c241d949e05f07e4e630db6dd893eaec1
Vũ,Danny D <d.vu@student.fontys.nl>
2021-09-24 11:55:50 +0200

    Add ESLint

---
+++ js/res/styleConstants.js
@@ -0,0 +1,36 @@
+'use strict';
+
+export const COLORS = {
+  WHITE: '#FFF',
+  BLACK: '#000',
+  WHITE20: '#FFFFFF33',
+};
+export const FOOTERHEIGHT = 170;
+export const HEADHERHEIGHT = 100;
+export const FONTCONSTANTS = {
+  fontFamily: 'Poppins',
+  fontSizeNormal: 18,
+};
+
+export const IOSSIZES = {
+  iPhoneXHeight: 812,
+  iPhoneXSHeight: 896,
+  iOSTopPadding: 20,
+  iPhoneXBottomPadding: 25,
+};
+
+export const BUTTONCONSTANTS = {
+  height: 60,
+  width: 130,
+  borderRadius: 10,
+};
+
+export const OPACITYVALUES = {
+  100: 1,
+  90: 0.9,
+  80: 0.8,
+  70: 0.7,
+  60: 0.6,
+  50: 0.5,
+  0: 0,
+};

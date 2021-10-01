5997d85ab774e256513735d298a352fe08226e98
Vũ,Danny D <d.vu@student.fontys.nl>
2021-10-01 10:55:25 +0200

    Removed warning

--- App.js
+++ App.js
@@ -1,3 +1,9 @@
+import { YellowBox } from 'react-native';
+
+YellowBox.ignoreWarnings([
+  'Warning: Cannot update during an existing state transition',
+]);
+
 var ViroSample = require('./js/Frame');
 
 module.exports = ViroSample;
